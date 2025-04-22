f77ae750a9baeb7b407b6c671010a23235583249
Niccolò Ciavarelli <152533242+ciavanick@users.noreply.github.com>
2025-04-22 13:01:19 +0200

    Added Tritium, Helium3 and 4 in Spectra analysis

--- CrunSpectrum.cpp
+++ CrunSpectrum.cpp
@@ -3,9 +3,18 @@
 
 void runSpectrum::initHistos()
 {
-    mHProtonSpectrum = new TH1D("mHProtonSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 62, 0.4, 3.5);
-    mHNeutronSpectrum = new TH1D("mHNeutronSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 62, 0.4, 3.5);
-    mHDeuteronSpectrum = new TH1D("mHDeuteronSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 31, 0.4, 3.5);
+    mHProtonSpectrum = new TH1D("mHProtonSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHNeutronSpectrum = new TH1D("mHNeutronSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHDeuteronSpectrum = new TH1D("mHDeuteronSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHTritiumSpectrum = new TH1D("mHTritiumSpectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHHelium3Spectrum = new TH1D("mHHelium3Spectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHHelium4Spectrum = new TH1D("mHHelium4Spectrum" + mName, "Spectrum;p_{T} (GeV/c)", 100, 0., 10.);
+    mHProtonSpectrumA = new TH1D("mHProtonSpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
+    mHNeutronSpectrumA = new TH1D("mHNeutronSpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
+    mHDeuteronSpectrumA = new TH1D("mHDeuteronSpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
+    mHTritiumSpectrumA = new TH1D("mHTritiumSpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
+    mHHelium3SpectrumA = new TH1D("mHHelium3SpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
+    mHHelium4SpectrumA = new TH1D("mHHelium4SpectrumA" + mName, "Spectrum;p_{T}/A (GeV/c)", 100, 0., 10.);
 }
 //_________________________________________________________________________
 void runSpectrum::initEventsHisto()
@@ -17,6 +26,9 @@ void runSpectrum::initEventsHisto()
     mEvents->Fill("Selected Protons", 0);
     mEvents->Fill("Selected Neutrons", 0);
     mEvents->Fill("Selected Deuterons", 0);
+    mEvents->Fill("Selected Tritons", 0);
+    mEvents->Fill("Selected Helium3", 0);
+    mEvents->Fill("Selected Helium4", 0);
 }
 //_________________________________________________________________________
 void runSpectrum::process()
@@ -34,12 +46,27 @@ void runSpectrum::process()
             {
                 mEvents->Fill("Selected Protons", 1);
                 mHProtonSpectrum->Fill(p.q[ipdg].Pt());
+                mHProtonSpectrumA->Fill(p.q[ipdg].Pt());
             }else if(p.pdgOptions[ipdg] == mPDGNe){
                 mEvents->Fill("Selected Neutrons", 1);
                 mHNeutronSpectrum->Fill(p.q[ipdg].Pt());
+                mHNeutronSpectrumA->Fill(p.q[ipdg].Pt());
             }else if(p.pdgOptions[ipdg] == mPDGDe){
                 mEvents->Fill("Selected Deuterons", 1);
                 mHDeuteronSpectrum->Fill(p.q[ipdg].Pt());
+                mHDeuteronSpectrumA->Fill(p.q[ipdg].Pt()/2);
+            }else if(p.pdgOptions[ipdg] == mPDGT){
+                mEvents->Fill("Selected Tritons", 1);
+                mHTritiumSpectrum->Fill(p.q[ipdg].Pt());
+                mHTritiumSpectrumA->Fill(p.q[ipdg].Pt()/3);
+            }else if(p.pdgOptions[ipdg] == mPDGHe3){
+                mEvents->Fill("Selected Helium3", 1);
+                mHHelium3Spectrum->Fill(p.q[ipdg].Pt());
+                mHHelium3SpectrumA->Fill(p.q[ipdg].Pt()/3);
+            }else if(p.pdgOptions[ipdg] == mPDGHe4){
+                mEvents->Fill("Selected Helium4", 1);
+                mHHelium4Spectrum->Fill(p.q[ipdg].Pt());
+                mHHelium4SpectrumA->Fill(p.q[ipdg].Pt()/4);
             }
         }
     }
@@ -50,13 +77,22 @@ void runSpectrum::writeHistos()
     mHProtonSpectrum->Write();
     mHNeutronSpectrum->Write();
     mHDeuteronSpectrum->Write();
+    mHTritiumSpectrum->Write();
+    mHHelium3Spectrum->Write();
+    mHHelium4Spectrum->Write();
+    mHProtonSpectrumA->Write();
+    mHNeutronSpectrumA->Write();
+    mHDeuteronSpectrumA->Write();
+    mHTritiumSpectrumA->Write();
+    mHHelium3SpectrumA->Write();
+    mHHelium4SpectrumA->Write();
 }
 //_________________________________________________________________________
 int runSpectrum::selectP(const particleCand &p)
 {
     for (int i = 0; i < p.pdgOptions.size(); ++i)
     {
-        if (p.pdgOptions[i] == mPDGPr || p.pdgOptions[i] == mPDGNe || p.pdgOptions[i] == mPDGDe)
+        if (p.pdgOptions[i] == mPDGPr || p.pdgOptions[i] == mPDGNe || p.pdgOptions[i] == mPDGDe || p.pdgOptions[i] == mPDGT || p.pdgOptions[i] == mPDGHe3 || p.pdgOptions[i] == mPDGHe4)
         {
             return i;
         }
--- CrunSpectrum.h
+++ CrunSpectrum.h
@@ -13,6 +13,16 @@ class runSpectrum : public vrun
       TH1D *mHProtonSpectrum = nullptr;
       TH1D *mHNeutronSpectrum = nullptr;
       TH1D *mHDeuteronSpectrum = nullptr;
+      TH1D *mHTritiumSpectrum = nullptr;
+      TH1D *mHHelium3Spectrum = nullptr;
+      TH1D *mHHelium4Spectrum = nullptr;
+      TH1D *mHProtonSpectrumA = nullptr;
+      TH1D *mHNeutronSpectrumA = nullptr;
+      TH1D *mHDeuteronSpectrumA = nullptr;
+      TH1D *mHTritiumSpectrumA = nullptr;
+      TH1D *mHHelium3SpectrumA = nullptr;
+      TH1D *mHHelium4SpectrumA = nullptr;
+
       void initHistos() override;
       void initEventsHisto() override;
       void process() override;
@@ -23,6 +33,10 @@ class runSpectrum : public vrun
       int mPDGPr = 2212;
       int mPDGNe = 2112;
       int mPDGDe = 4324;
+      int mPDGT = 2212 + 2*2112;
+      int mPDGHe3 = 2*2212 + 2112;
+      int mPDGHe4 = 2*2212 + 2*2112;
+
 };
 
 #endif
