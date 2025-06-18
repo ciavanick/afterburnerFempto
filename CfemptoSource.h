648ef942a5faf4c1d2dc4d61050fd217a8a2a56f
Niccolò Ciavarelli <152533242+ciavanick@users.noreply.github.com>
2025-06-18 22:28:07 +0200

    femtosource only for wigner function

--- CfemptoSource.h
+++ CfemptoSource.h
@@ -2,33 +2,48 @@
 #define CFEMPTOSOURCE
 
 #include "Cvfempto.h"
-#include "CwaveUtils.h"
+#include "CWignerUtils.h"
 #include "TF1.h"
 #include "TF2.h"
 
 class femptoSource : public vfempto
 {
   public:
-    femptoSource() { waveUtils::setParams(17.4, 3.2, 1.44, -3); } // here we work with MeV (Rsource=-1 means -> wave function defined by setting Rsource to match K.E.)
-    void setParams(float strong=2.2E-3, float strongR=2.4, float coulomb=1.44E-3, float sourceRadius=0, float spinFact=3./8) { waveUtils::setParams(strong, strongR, coulomb, sourceRadius, spinFact); }
+    femptoSource() {
+        wignerUtils::setParams(17.4E-3, 3.2, 1.44E-3, -3); // units in GeV (e.g., 17.4 MeV = 0.0174 GeV)
+    }
+
+    void setParams(float strong = 17.4E-3, float strongR = 3.2, float coulomb = 1.44E-3,
+                   float sourceRadius = 0, float spinFact = 3. / 8) {
+        wignerUtils::setParams(strong, strongR, coulomb, sourceRadius, spinFact);
+    }
+
     bool set(particleMC& p1, particleMC& p2, float chargeColoumb, float chargeStrong);
-    double doInteract(particleMC& p1, particleMC& p2, float chargeColoumb, float chargeStrong, float sumRadii = 0, float *pos=nullptr, float *posLab=nullptr); // perform interaction and return momentum exchanged
-    virtual float getCoalProb(const particleMC& p1, const particleMC& p2) { return waveUtils::getCoalProb(p1, p2); }
-    float calcProb() { return waveUtils::calcProb(); }
+    double doInteract(particleMC& p1, particleMC& p2, float chargeColoumb, float chargeStrong,
+                      float sumRadii = 0, float* pos = nullptr, float* posLab = nullptr);
+
+    virtual float getCoalProb(const particleMC& p1, const particleMC& p2);
+    float calcProb() { return wignerUtils::getcoal(); }
+
     virtual void init();
 
-    void setSourceRadius(float radius) { waveUtils::setSourceRadius(radius); }
-    void setKstar(float kstar, float kt=1.0, waveUtils::type system=waveUtils::nn) { waveUtils::setKstar(kstar, kt, system); }
-    void setCharges(float cS, float cC) { waveUtils::setCharges(cS, cC); }
+    void setSourceRadius(float radius) { wignerUtils::setSourceRadius(radius); }
+    void setKstar(float kstar, float kt = 1.0) {
+        wignerUtils::setKstar(kstar, kt);
+    }
 
-    float kineticSource() const { return waveUtils::kineticSource(); }
-    float potentialSource() const { return waveUtils::potentialSource(); }
-    float getKstarFinal(float coalProb = 0., float massRed=waveUtils::MRED_NN, float boundE=waveUtils::EBOUND_D) const { return waveUtils::getKstarFinal(coalProb, massRed, boundE); }
-    float kineticDeuteron() const { return waveUtils::kineticDeuteron(); }
-    float potentialDeuteron() const { return waveUtils::potentialDeuteron(); }
+    void setCharges(float cS, float cC) {
+        wignerUtils::setParams(cS, 3.2, cC, wignerUtils::getRadius()); // 3.2 = strongR default
+    }
 
-  private:
+    float kineticSource() const { return wignerUtils::kineticSource(); }
+    float potentialSource() const { return wignerUtils::potentialSource(); }
+
+    float getKstarFinal(float coalProb = 0., float massRed = 938. / 2., float boundE = 2.22) const;
+
+    float kineticDeuteron() const { return 0; }     // Placeholder
+    float potentialDeuteron() const { return 0; }   // Placeholder
+
 };
 
 #endif
-
